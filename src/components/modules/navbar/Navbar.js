817d3c9961470e49c35f1edb333aa8de260e77b8
Milad Joodi <winbeta.com@gmail.com>
2024-03-16 19:18:51 +0330

    sticky navbar

--- src/components/modules/navbar/Navbar.js
+++ src/components/modules/navbar/Navbar.js
@@ -1,12 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./Nabvar.module.css";
 import Link from "next/link";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaShoppingCart, FaRegHeart } from "react-icons/fa";
 
 function Navbar() {
+  const [fixTop, setFixTop] = useState(false);
+
+  useEffect(() => {
+    const fixNavbarToTop = () => {
+      const currentScroll = window.pageYOffset;
+      if (currentScroll > 105) {
+        setFixTop(true);
+      } else {
+        setFixTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", fixNavbarToTop);
+
+    return () => window.removeEventListener("scroll", fixNavbarToTop);
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={fixTop ? styles.navbar_fixed : styles.navbar}>
       <main>
         <div>
           <Link href="/">
