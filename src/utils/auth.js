ec442d8456accda02d8fdaf024696e198a2c5073
Milad Joodi <winbeta.com@gmail.com>
2024-09-07 21:48:23 +0330

    up

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -40,10 +40,27 @@ const generateRefreshToken = (data) => {
   return token;
 };
 
+// validate email
+const validateEmail = (email) => {
+  const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+  return pattern.test(email);
+};
+const validatePhone = (phone) => {
+  const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+  return pattern.test(phone);
+};
+const validatePassword = (password) => {
+  const pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  return pattern.test(password);
+};
+
 export {
   hashPassword,
   verifyPassword,
   generateAccessToken,
   verifyAccessToken,
   generateRefreshToken,
+  validateEmail,
+  validatePhone,
+  validatePassword,
 };
